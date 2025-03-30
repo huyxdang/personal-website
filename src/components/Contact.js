4f9ce3a665c86451b1d23be7faadcda49b77a084
Huy Dang <xhuydng@gmail.com>
2025-03-30 19:32:17 -0400

    ok i'm done for the day

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -60,8 +60,7 @@ function Contact() {
           Don't like my website? Don't like my work?
         </p>
 
-        <p style={{ fontSize: "1rem", color: "#ccc", marginBottom: "1rem", 
-        textDecoration: "underline", textUnderlineOffset: "4px" }}>
+        <p style={{ fontSize: "1rem", color: "#ccc", marginBottom: "1rem", fontWeight: "bold" }}>
           Don't even like me as a person?
         </p>
 
