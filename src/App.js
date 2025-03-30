f7a8a517e7cbc1fbd7e758362bf37c67dd2feac4
Huy Dang <xhuydng@gmail.com>
2025-03-29 20:17:05 -0400

    some changes to App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { Link, Element } from 'react-scroll';
+import Hero from './components/Hero';
+import About from './components/About';
+import Projects from './components/Projects';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Nav */}
+      <nav>
+        <ul>
+          <li>
+            <Link to="about" smooth={true} duration={500}>About</Link>
+          </li>
+          <li>
+            <Link to="projects" smooth={true} duration={500}>Projects</Link>
+          </li>
+        </ul>
+      </nav>
+
+      {/* Sections */}
+      <Element name="hero">
+        <Hero />
+      </Element>
+
+      <Element name="about">
+        <About />
+      </Element>
+
+      <Element name="projects">
+        <Projects />
+      </Element>
     </div>
   );
 }
